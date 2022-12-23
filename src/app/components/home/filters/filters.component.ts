5c926e926c19960b858f9d6784c13264c54bc8b5
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2022-12-23 11:41:04 -0300

    Filter compoment created

--- src/app/components/home/filters/filters.component.ts
+++ src/app/components/home/filters/filters.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { CtgovApiService } from 'src/app/services/ctgov-api.service';
+import { STATUSES } from '../statuses';
 
 @Component({
   selector: 'app-filters',
@@ -7,20 +9,36 @@ import { CtgovApiService } from 'src/app/services/ctgov-api.service';
   styleUrls: ['./filters.component.scss']
 })
 export class FiltersComponent implements OnInit {
-  numTrials = 0;
+  
+  public numTrials: number = 0;
+  public statusesList: string[] = STATUSES;
+  public statusFilter: string;
+  public completitionDate = new FormGroup({
+    primaryCompletionDateFrom: new FormControl(),
+    primaryCompletionDateUntil: new FormControl()
+  });
 
   constructor(
     private ctgovService: CtgovApiService
   ) { }
 
   ngOnInit(): void {
-    this.ctgovService.loadTrialData();  
+    this.getTrialData();
   }
 
   getTrialData() {
+    this.ctgovService.loadTrialData(this.statusFilter, null, this.formatDate(this.completitionDate.value.primaryCompletionDateFrom), this.formatDate(this.completitionDate.value.primaryCompletionDateUntil));
+
     this.ctgovService.numTrials$.subscribe(numTrials => {
       this.numTrials = numTrials;
     });
+  }
+
+  private formatDate(date: Date): string {
+    if (date)
+      return date.getFullYear() + '%2F' + (date.getMonth() + 1) + '%2F' + date.getDay();
+
+    return null;
   }
 
 }
