01d6f02ed3e69aaa5b94c0269a6da8af2234d965
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2022-12-23 11:44:08 -0300

    Angular material installed; basic configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FiltersComponent } from './components/home/filters/filters.component';
 import { TableComponent } from './components/home/table/table.component';
+import { MaterialModule } from './material.module';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,17 @@ import { TableComponent } from './components/home/table/table.component';
     TableComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
