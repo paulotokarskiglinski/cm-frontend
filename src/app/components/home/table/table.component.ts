e3c9dd47731ac2b7e3c7b657e1fd820ad14a2289
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2022-12-23 11:42:41 -0300

    Table implemented

--- src/app/components/home/table/table.component.ts
+++ src/app/components/home/table/table.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { CtgovApiService } from 'src/app/services/ctgov-api.service';
+import { CTGovItem } from '../interfaces';
+
+const ELEMENT_DATA: CTGovItem[] = [];
 
 @Component({
   selector: 'app-table',
@@ -7,10 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor(
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  public numTrials: number = 0;
+  public trials = new MatTableDataSource<CTGovItem>(ELEMENT_DATA);
+  public displayedColumns: string[] = ['Rank', 'NCTId', 'BriefTitle', 'Condition', 'OverallStatus', 'PrimaryCompletionDate'];
+
+  constructor(
+    private ctgovService: CtgovApiService
   ) { }
 
-  ngOnInit(): void { }
+  ngAfterViewInit() {
+    this.trials.paginator = this.paginator;
+  }
+
+  ngOnInit(): void {
+    this.getTrialData();
+  }
+
+  public getTrialData(): void {
+    this.ctgovService.numTrials$.subscribe(numTrials => {
+      this.numTrials = numTrials;
+    });
+
+    this.ctgovService.trials$.subscribe(trials => {
+      this.trials.data = trials;
+      console.log(this.trials)
+    });
+  }
 
 }
